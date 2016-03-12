52deee26a4ab8d2245e781daa653f13bf1c034bd
Alejandro <el_lalinhom@hotmail.com>
2016-03-12 17:39:06 +0100

    modificaciones alumno 1

--- practicaGIT/GitAux.java
+++ practicaGIT/GitAux.java
@@ -11,20 +11,19 @@ package prgitejemplo;
 public class GitAux {
     
     public void metodoAlumno1(){
-     
-        System.out.println("Este método está mal");
+        System.out.println("metodo 1");
+        System.out.println("modificado por alumno 1");
     }
     
     public void metodoAlumno2(){
-        System.out.println("metodo n");
+        System.out.println("metodo 2");
     }
     
     public void metodoComunitario(){
+        //Comentario incluido para pruebas
         System.out.println("Aqui escribimos todos");
+        // modificaciones alumno 1
+        System.out.println("Alumno 1 inserta esta sentencia");
     }
-    
-    public void testGitAux(){
-        // metodo rama test
-        System.out.println("Metodo de test");
-    }
+  
 }
