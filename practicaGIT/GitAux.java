d3cbeb0bfe0ded749c7226f5d0a6c95c20d46367
Alejandro <el_lalinhom@hotmail.com>
2016-03-12 16:37:47 +0100

    Primera modificación

---
+++ practicaGIT/GitAux.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prgitejemplo;
+
+/**
+ *
+ * @author bertoa
+ */
+public class GitAux {
+    
+    public void metodoAlumno1(){
+     
+        System.out.println("Este método está mal");
+    }
+    
+    public void metodoAlumno2(){
+        System.out.println("metodo n");
+    }
+    
+    public void metodoComunitario(){
+        System.out.println("Aqui escribimos todos");
+    }
+}
